7aafa6e3eb4ccdceaec85c5f7727cf358a038495
MironIrk <mironkct@gmail.com>
2023-02-24 18:54:57 +0800

    ready app

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import { API_KEY } from "./utils.js";
 
+
 // получаю кнопку и поле ввода города
 const cityInput = document.getElementById('city-input');
 const searchButton = document.getElementById('search-button');
@@ -27,11 +28,26 @@ const temperatureData = [];
 const chartLabels = [];
 const labels = [];
 
+// эта функция добавляет прослушиватель событий по щелчку мыши на кнопке поиска
+// и переключения между вкладками
 function addListeners() {
-  // todo
+  searchButton.addEventListener('click', () => {
+    const city = cityInput.value;
+    getWeatherData(city);
+  });
+
+  tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+      tabs.forEach(tab => {
+        tab.classList.remove('active');
+      });
+      tab.classList.add('active');
+      updateChart(index);
+    })
+  })
 }
 
-// извлекaю данные из openweathermap API
+// извлекaю данные из openweathermap по API
 // первый запрос дает нам текущие данные о погоде
 // второй запрос даст нам прогнозные данные
 async function fetchData(city) {
@@ -46,22 +62,115 @@ async function fetchData(city) {
 
   const currentWeatherData = await currentWeatherResponse.json();
   const forecastData = await forecastResponse.json();
-
+  console.log(currentWeatherData, forecastData)
   return [currentWeatherData, forecastData];
 }
 
 function drawChart() {
+  // сначала импортируем библиотеку chart js через CDN для построение графиков
 
+  const chart = new Chart(chartContainer, {
+    type: 'line',
+    data: {
+      labels: chartLabels[0],
+      datasets: [
+        {
+          label: 'Temperature',
+          data: temperatureData[0],
+          borderColor: 'rgba(24, 90, 157, 1)',
+          borderWidth: 1,
+          pointRadius: 5,
+          pointHoverRadius: 10,
+          fill: true
+        }
+      ]
+    },
+  });
+
+  window.chart = chart;
 }
 
-function updateText() {
-  // todo
+function updateText(city, currentWeatherData) {
+  console.log(city, currentWeatherData)
+  cityDisplay.textContent = city;
+  temperatureDisplay.textContent = `${currentWeatherData.main.temp}°C`;
+  weatherTypeDisplay.textContent = `${currentWeatherData.weather[0].main}`;
+  feelsLikeDisplay.textContent = `По ощущению:  ${currentWeatherData.main.feels_like}°C`;
+  humidityDisplay.textContent = `${currentWeatherData.main.humidity} %`;
+  windspeedDisplay.textContent = `${currentWeatherData.wind.speed} м/с`;
+  const time = new Date().toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false
+  });
+  const date = new Date().toLocaleDateString('ru-RU', {
+    weekday: 'short',
+    month: 'short',
+    day: 'numeric'
+  });
+  dateDisplay.textContent = date + ' ' + time;
+
+  weatherIcon.src = `http://openweathermap.org/img/wn/${currentWeatherData.weather[0].icon}@2x.png`;
 }
 
 async function getWeatherData(city) {
+
+  // сброс;ываю массивы для каждого нового города
+  temperatureData.length = 0;
+  labels.length = 0;
+  chartLabels.length = 0;
+
   const [currentWeatherData, forecastData] = await fetchData(city);
-  console.log([currentWeatherData, forecastData])
+
+  /** 
+   * здесь мы перебираем данные прогноза погоды и 
+   * заполняем наши массивы температурных данных для диаграммы
+   * */
+  forecastData.list.forEach((item, i) => {
+    let date = new Date(item.dt * 1000);
+    let dateString = date.toLocaleDateString();
+
+    if (i === 0 || dateString !== new Date(forecastData.list[i - 1].dt * 1000).toLocaleDateString()) {
+      console.log(item.main.temp)
+      temperatureData.push([item.main.temp]);
+      labels.push(date.toLocaleDateString('ru-RU', {
+        weekday: 'short',
+        month: 'short',
+        day: 'numeric'
+      }));
+      chartLabels.push([date.toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false
+      })]);
+    } else {
+      temperatureData[temperatureData.length - 1].push(item.main.temp);
+      chartLabels[chartLabels.length - 1].push(date.toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false
+      }));
+    }
+
+  });
+
+  updateText(city, currentWeatherData);
+  drawChart();
+
+  // делает первую вкладку активной при выборе нового города и
+  // обновляются даты до актуальных в вкладках данных прогноза погоды
+  tabs[0].classList.add('active');
+  tabs.forEach((tab, index) => {
+    tab.textContent = labels[index];
+  });
 
 }
 
-getWeatherData('Moscow');
+function updateChart(index) {
+  chart.data.datasets[0].data = temperatureData[index];
+  chart.data.labels = chartLabels[index];
+  chart.update();
+}
+
+addListeners();
+getWeatherData('Irkutsk');
